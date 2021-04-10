ae30f15ba370729b69f046bf090fc579c72a9061
Nishant <nishantb1130@gmail.com>
2021-04-09 22:26:09 -0700

    basic map and app.jsx update

--- frontend/src/components/map.component.jsx
+++ frontend/src/components/map.component.jsx
@@ -1,1 +1,28 @@
-import React from 'react'; 
+import React, { useState } from 'react';
+import ReactMapGL from 'react-map-gl'; 
+
+const Map = () => {
+
+    const MAPBOX_TOKEN = 'pk.eyJ1IjoibmlzaGFudGJhbGFqaSIsImEiOiJja2xkOGl3cjcxc21yMndtdmxtZWpxeGRuIn0.isOPq2BjpvuzwjZMXW1yWA'; //process.env.MAPBOX_TOKEN; 
+
+    const [viewport, setViewport] = useState({
+        width: "100vw",
+        height: "100vh",
+        latitude: 37.7577,
+        longitude: -122.4376,
+        zoom: 8
+      });
+
+    return(
+        <div>
+           <ReactMapGL
+            {...viewport}
+            onViewportChange={nextViewport => setViewport(nextViewport)}
+            mapboxApiAccessToken={MAPBOX_TOKEN}
+            mapStyle="mapbox://styles/mapbox/streets-v11"
+            />
+        </div>
+    )
+}
+
+export default Map; 
