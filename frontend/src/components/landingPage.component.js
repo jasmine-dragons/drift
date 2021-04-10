928586dda764296434f952cd61f05636e9ee1d30
PrateekS1917 <61072843+PrateekSane@users.noreply.github.com>
2021-04-10 13:36:52 -0700

    toggle pages

---
+++ frontend/src/components/landingPage.component.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../common.css";
+
+const LandingPage = (props) => {
+  return (
+    <div className="container">
+      <img
+        className="main-background"
+        src="pages/background.png"
+        alt="background"
+      />
+      <img className="main-logo" src="driftlogo.svg" alt="drift-logo" />
+      <p className="main-start-adventure-text">your next adventure awaits</p>
+      <button className="main-login-button" onClick={props.updatePage}>
+        {" "}
+        sign up{" "}
+      </button>
+      <button className="main-login-button-2"> login </button>
+    </div>
+  );
+};
+
+export default LandingPage;
